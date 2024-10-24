3a024e1f6e7acf299d86f9611bf78f90bb32af54
joaopavila <pavilajoao@gmail.com>
2024-10-23 23:13:29 -0300

    chore: start create component avatar

--- ff.web/src/components/pages/ProfilePage.tsx
+++ ff.web/src/components/pages/ProfilePage.tsx
@@ -1,29 +1,84 @@
-// @ts-nocheck
-
-import Page from "@vuo/templates/Page";
-import Button from "@vuo/atoms/Button";
 import { ThemeContext } from "@vuo/context/ThemeContext";
-import { useContext, useEffect, useState } from "react";
 import useStackNavigator from "@vuo/hooks/StackNavigator";
+import Page from "@vuo/templates/Page";
+import { useContext, useEffect, useState } from "react";
+import { Avatar } from "../atoms/Avatar";
+
+import Button from "../atoms/Button";
+import { Tabs } from "../molecules/Tabs";
+import { UserPreferences } from "../organisms/userPreferences";
+import styles from "./ProfilePage.module.scss";
 
 const ProfilePage = function () {
   const { toggleTheme } = useContext(ThemeContext);
-  const [profileData, setProfileData] = useState<any>(null);
+  const [profileData, setProfileData] = useState<any>(null); // Ensure it's typed correctly
   const { navigateWithState } = useStackNavigator();
 
   useEffect(() => {
     const storedProfile = localStorage.getItem("profileData");
     if (storedProfile) {
       const parsedProfile = JSON.parse(storedProfile);
-      delete parsedProfile.completedSteps;
-      setProfileData(parsedProfile);
+      delete parsedProfile.completedSteps; // Remove the completedSteps key
+      setProfileData(parsedProfile); // Set the modified profile data
     }
   }, []);
 
+  //TODO fix this creappy UI
+
   return (
     <Page>
-      <h2>Hello Person</h2>
-      {profileData && (
+      <div className={styles.profilePage__header}>
+        <div className={styles.profilePage__header__avatar}>
+          <Avatar src="https://placehold.co/50x50" alt="Image profile" />
+          <div className={styles.profilePage__avatarInfo}>
+            <p className={styles.profilePage__avatarInfo__name}>Shawn</p>
+            <p className={styles.profilePage__avatarInfo__role}>Umami Master</p>
+          </div>
+        </div>
+
+        <Button
+          variant="small"
+          color="primary"
+          onClick={() => {
+            navigateWithState("/onboarding");
+          }}
+        >
+          Edit Profile
+        </Button>
+      </div>
+      <Tabs
+        tabs={[
+          {
+            id: "tab1",
+            label: "YOUR PROFILE",
+            content: (
+              <UserPreferences
+                listOfAllergies={profileData?.allergies}
+                listOfDiets={profileData?.diets}
+                listOfCuisinePreferences={profileData?.cuisinePreferences}
+              />
+            ),
+          },
+          { id: "tab2", label: "FOOD PROFILE", content: <div>Content 2</div> },
+        ]}
+      />
+
+      <div className={styles.profilePage__footer}>
+        <Button
+          variant="large"
+          color="primary"
+          onClick={() => {
+            toggleTheme();
+          }}
+        >
+          Change Theme
+        </Button>
+        <Button variant="small" color="secondary">
+          Delete account
+        </Button>
+      </div>
+
+      {/* {profileData && (
         <div>
           <h4>Your Dietary Preferences</h4>
           {Object.entries(profileData).map(([key, value]) => {
@@ -35,41 +90,20 @@ const ProfilePage = function () {
             return (
               <div key={key} style={{ marginBottom: "1rem" }}>
                 <strong>{key.charAt(0).toUpperCase() + key.slice(1)}:</strong>
-                {Array.isArray(value) && (
+                {Array.isArray(value) ? (
                   <ul>
                     {value.map((item, index) => (
                       <li key={index}>{item}</li>
                     ))}
                   </ul>
+                ) : (
+                  <p>{value}</p>
                 )}
               </div>
             );
           })}
         </div>
-      )}
-
-      {/* Optional Theme Button */}
-      <Button
-        variant="large"
-        color="primary"
-        onClick={() => {
-          navigateWithState("/onboarding");
-        }}
-      >
-        Edit
-      </Button>
-      <Button
-        tabIndex={0}
-        block
-        variant="large"
-        type="submit"
-        color="primary"
-        onClick={() => {
-          toggleTheme();
-        }}
-      >
-        Change Theme
-      </Button>
+      )} */}
     </Page>
   );
 };
