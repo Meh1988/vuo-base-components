5215abb267c07b75879129d3a038be8c3516d7fc
joaopavila <pavilajoao@gmail.com>
2024-11-03 15:35:27 -0300

    chore: correct image food profile

--- ff.web/src/components/organisms/userFoodProfile/UserFoodProfile.tsx
+++ ff.web/src/components/organisms/userFoodProfile/UserFoodProfile.tsx
@@ -3,13 +3,18 @@ import Section from "@vuo/components/atoms/Section";
 import { StarRating } from "@vuo/components/molecules/StarRating";
 import styles from "./UserFoodProfile.module.scss";
 
+import Day1BaconImage from "../../../../public/images/1.webp";
+import Day2KetoImage from "../../../../public/images/2.webp";
+
 export const UserFoodProfile = () => {
   return (
     <div className={styles.userFoodProfile}>
       <Section className={styles.userFoodProfile__section}>
-        <div className={styles.userFoodProfile__img}>
-          {/* <img src="https://placehold.co/50x50" alt="Image profile" /> */}
-        </div>
+        <img
+          className={styles.userFoodProfile__img}
+          src={Day1BaconImage}
+          alt="Image profile"
+        />
         <div className={styles.userFoodProfile__info}>
           <p className={styles.userFoodProfile__title}>
             Grill chicken avocado salad
@@ -23,9 +28,11 @@ export const UserFoodProfile = () => {
         </div>
       </Section>
       <Section className={styles.userFoodProfile__section}>
-        <div className={styles.userFoodProfile__img}>
-          {/* <img src="https://placehold.co/50x50" alt="Image profile" /> */}
-        </div>
+        <img
+          className={styles.userFoodProfile__img}
+          src={Day2KetoImage}
+          alt="Image profile"
+        />
         <div className={styles.userFoodProfile__info}>
           <p className={styles.userFoodProfile__title}>
             Mexican cauliflower rice
