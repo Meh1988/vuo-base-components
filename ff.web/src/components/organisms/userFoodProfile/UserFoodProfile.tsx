6697c014af5e03b18addf756edefabc33cc2e9f3
joaopavila <pavilajoao@gmail.com>
2024-11-03 12:15:47 -0300

    chore: create component star and create page user food profile

--- ff.web/src/components/organisms/userFoodProfile/UserFoodProfile.tsx
+++ ff.web/src/components/organisms/userFoodProfile/UserFoodProfile.tsx
@@ -1,5 +1,6 @@
-import { HeartFilled } from "@ant-design/icons";
+import { HeartFilled, ShoppingCartOutlined } from "@ant-design/icons";
 import Section from "@vuo/components/atoms/Section";
+import { StarRating } from "@vuo/components/molecules/StarRating";
 import styles from "./UserFoodProfile.module.scss";
 
 export const UserFoodProfile = () => {
@@ -10,9 +11,15 @@ export const UserFoodProfile = () => {
           {/* <img src="https://placehold.co/50x50" alt="Image profile" /> */}
         </div>
         <div className={styles.userFoodProfile__info}>
-          <p>Grill chicken avocado salad</p>
-          <HeartFilled />
-          <p>Very Good</p>
+          <p className={styles.userFoodProfile__title}>
+            Grill chicken avocado salad
+          </p>
+          <div className={styles.userFoodProfile__icons}>
+            <HeartFilled />
+            <ShoppingCartOutlined />
+          </div>
+          <p className={styles.userFoodProfile__rating}>Very Good</p>
+          <StarRating />
         </div>
       </Section>
       <Section className={styles.userFoodProfile__section}>
@@ -20,9 +27,15 @@ export const UserFoodProfile = () => {
           {/* <img src="https://placehold.co/50x50" alt="Image profile" /> */}
         </div>
         <div className={styles.userFoodProfile__info}>
-          <p>Mexican cauliflower rice</p>
-          <HeartFilled />
-          <p>Very Good</p>
+          <p className={styles.userFoodProfile__title}>
+            Mexican cauliflower rice
+          </p>
+          <div className={styles.userFoodProfile__icons}>
+            <HeartFilled />
+            <ShoppingCartOutlined />
+          </div>
+          <p className={styles.userFoodProfile__rating}>Very Good</p>
+          <StarRating />
         </div>
       </Section>
     </div>
