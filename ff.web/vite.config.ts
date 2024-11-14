d8e9cbc3ce82f1b474f5e5d225ebdda9206b7cee
Adam Gerhes <adamg@vuo.ai>
2024-11-14 14:48:00 +0200

    env variables defined in vite config

--- ff.web/vite.config.ts
+++ ff.web/vite.config.ts
@@ -25,6 +25,14 @@ export default () => {
     base: BASE_URL,
     define: {
       API_URL: JSON.stringify(API_URL),
+      'import.meta.env.VITE_FIREBASE_WEB_API_KEY': JSON.stringify(process.env.VITE_FIREBASE_WEB_API_KEY),
+      'import.meta.env.VITE_FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.VITE_FIREBASE_AUTH_DOMAIN),
+      'import.meta.env.VITE_FIREBASE_PROJECT_ID': JSON.stringify(process.env.VITE_FIREBASE_PROJECT_ID),
+      'import.meta.env.VITE_FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.VITE_FIREBASE_STORAGE_BUCKET),
+      'import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.VITE_FIREBASE_MESSAGING_SENDER_ID),
+      'import.meta.env.VITE_FIREBASE_APP_ID': JSON.stringify(process.env.VITE_FIREBASE_APP_ID),
+      'import.meta.env.VITE_FIREBASE_MEASUREMENT_ID': JSON.stringify(process.env.VITE_FIREBASE_MEASUREMENT_ID),
+      'import.meta.env.VITE_FFAPI_BASE_URL': JSON.stringify(process.env.VITE_FFAPI_BASE_URL),
     },
     server: {
       port: PORT,
