766c4a947f680a7aa9b0ac4b599f29caeb812cb6
Adam Gerhes <adamg@vuo.ai>
2024-11-14 15:28:20 +0200

    still debugging

--- ff.web/src/config/firebase.ts
+++ ff.web/src/config/firebase.ts
@@ -2,7 +2,6 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getAnalytics } from 'firebase/analytics';
 
-console.log("secrets from firebase.ts", import.meta.env.VITE_FIREBASE_WEB_API_KEY);
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_WEB_API_KEY,
@@ -13,6 +12,7 @@ const firebaseConfig = {
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
+console.log("secrets from firebase.ts", firebaseConfig);
 
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app); 
