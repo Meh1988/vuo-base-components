0b06ca4df5d2cc3dd4a0e8ec8bbacbaed45da41e
joaopavila <pavilajoao@gmail.com>
2024-11-17 16:01:56 -0300

    chore: create skeleton loading

--- ff.web/src/App.tsx
+++ ff.web/src/App.tsx
@@ -1,12 +1,15 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 
 import { SafeArea } from "antd-mobile";
+import { getAnalytics, logEvent } from "firebase/analytics";
+import { useEffect } from "react";
 import BottomNavigation from "./components/organisms/BottomNavigation";
 import Navbar from "./components/organisms/Navbar";
 import OnboardingFlow from "./components/organisms/onboarding";
 import { EditProfile } from "./components/pages/EditProfile";
 import FlavourFlowPage from "./components/pages/FlavourFlowPage";
 import FlavourFlowResultPage from "./components/pages/FlavourFlowResultPage";
+import GamePlayerPage from "./components/pages/GamePlayerPage";
 import Home from "./components/pages/Home";
 import Login from "./components/pages/Login";
 import MealMap from "./components/pages/MealMap";
@@ -18,14 +21,9 @@ import QuestOutro from "./components/pages/QuestOutro";
 import QuestPlay from "./components/pages/QuestPlay";
 import QuestSelection from "./components/pages/QuestSelection";
 import ShoppingCart from "./components/pages/ShoppingCart";
+import { app } from "./config/firebase";
 import { ThemeProvider } from "./context/ThemeContext";
-import GamePlayerPage from './components/pages/GamePlayerPage';
 import AuthenticatedRoute from "./routeGuards/AuthenticetedRoute";
-import { useEffect } from "react";
-import { getAnalytics } from "firebase/analytics";
-import { app } from './config/firebase';
-import { logEvent } from "firebase/analytics";
-
 
 // Initialize Firebase Analytics
 const analytics = getAnalytics(app);
@@ -35,19 +33,19 @@ const GTM_ID = import.meta.env.VITE_GTM_ID;
 
 function initializeGTM() {
   if (!GTM_ID) {
-    console.warn('GTM ID not found in environment variables');
+    console.warn("GTM ID not found in environment variables");
     return;
   }
 
   // Add GTM Script
-  const script = document.createElement('script');
+  const script = document.createElement("script");
   script.async = true;
   script.src = `https://www.googletagmanager.com/gtm.js?id=${GTM_ID}`;
   document.head.appendChild(script);
 
   // Add noscript iframe for GTM
-  const noscript = document.createElement('noscript');
-  const iframe = document.createElement('iframe');
+  const noscript = document.createElement("noscript");
+  const iframe = document.createElement("iframe");
   iframe.src = `https://www.googletagmanager.com/ns.html?id=${GTM_ID}`;
   iframe.height = "0";
   iframe.width = "0";
@@ -57,16 +55,13 @@ function initializeGTM() {
   document.body.insertBefore(noscript, document.body.firstChild);
 }
 
-
-
 function App() {
   useEffect(() => {
     // Initialize GTM for ad tracking
     initializeGTM();
 
     // Log initial page view in Firebase Analytics
-    logEvent(analytics, 'page_view');
-
+    logEvent(analytics, "page_view");
   }, []);
 
   return (
@@ -92,18 +87,27 @@ function App() {
               path="/flavour-flow/results"
               element={<FlavourFlowResultPage />}
             />
-            <Route path="/flavour-flow" element={
-              <AuthenticatedRoute>
-                <FlavourFlowPage />
-              </AuthenticatedRoute>
-            } />
-            <Route path="/flavour-flow/results" element={
-              <AuthenticatedRoute>
-                <FlavourFlowResultPage />
-              </AuthenticatedRoute>
-            } />
+            <Route
+              path="/flavour-flow"
+              element={
+                <AuthenticatedRoute>
+                  <FlavourFlowPage />
+                </AuthenticatedRoute>
+              }
+            />
+            <Route
+              path="/flavour-flow/results"
+              element={
+                <AuthenticatedRoute>
+                  <FlavourFlowResultPage />
+                </AuthenticatedRoute>
+              }
+            />
             <Route path="/onboarding" element={<OnboardingFlow />} />
-            <Route path="/minigames/play/:gameId" element={<GamePlayerPage />} />
+            <Route
+              path="/minigames/play/:gameId"
+              element={<GamePlayerPage />}
+            />
             <Route path="/minigames" element={<Minigames />} />
             <Route path="*" element={<NotFoundPage />} />
           </Route>
