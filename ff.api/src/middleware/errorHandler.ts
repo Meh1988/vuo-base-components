86c9c083110975938d2f8c01c250340e87c47b9c
hiiragi1604 <devildan2005@gmail.com>
2024-11-18 14:22:50 +0200

    integrate AI generation features to API

--- ff.api/src/middleware/errorHandler.ts
+++ ff.api/src/middleware/errorHandler.ts
@@ -1,5 +1,13 @@
 import { NextFunction, Request, Response } from "express";
 
+/**
+ * Error handler middleware
+ * @param err: The error to handle
+ * @param req: The request
+ * @param res: The response
+ * @param next: The next function
+ * @returns The error message
+ */
 const errorHandler = (
   err: Error,
   req: Request,
